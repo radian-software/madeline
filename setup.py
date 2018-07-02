c48e794bb392157690650fc6de108a0a1c58ab12
Radon Rosborough <radon.neon@gmail.com>
2018-07-01 20:43:03 -0600

    First cut at local mirroring

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+# https://python-packaging.readthedocs.io/en/latest/minimal.html
+setup(
+    author="Radon Rosborough",
+    author_email="radon.neon@gmail.com",
+    description="Bidirectional sparse-tree directory mirroring.",
+    license="MIT",
+    install_requires=[],
+    name="madeline",
+    scripts=["mdl"],
+    url="https://github.com/raxod502/madeline",
+    version=None,
+)
